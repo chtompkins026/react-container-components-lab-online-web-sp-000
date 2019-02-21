f755f487e9026c8a1940186a9748d7a16f25e97f
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-21 17:35:22 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+// ADdUzSL1QAHJgEAJdPHxJYaU2hNcxkud
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0'; 
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${ADdUzSL1QAHJgEAJdPHxJYaU2hNcxkud}`;
+            + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+  
