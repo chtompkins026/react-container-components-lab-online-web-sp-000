1d8031b837002c2143dc3ce4b3802cd553a80fa2
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-21 17:30:49 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,9 @@
-// Code MovieReviews Here
+import React from 'react'; 
+
+const Review = ({headline, byline, link}) => {
+  return {
+    <div key={headline} className="review"> 
+      <a href={link}>{headline} - {byLine} </a> 
+    </div> 
+  }
+}
